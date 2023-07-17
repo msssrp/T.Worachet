c91541d57d01963383ca8e093668a2097308bb20
Mark <siripoomcontact@gmail.com>
2023-07-17 09:44:04 +0700

    work2

---
+++ work-2-RESTFul/main.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const PORT = 8080;
+const cors = require('cors')
+const sql = require('./model/db_connect')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+
+app.get('/', (req,res)=>{
+    res.send('hello')
+})
+
+app.listen(PORT,()=>{
+    console.log("server running on port :"+ PORT);
+})
+
